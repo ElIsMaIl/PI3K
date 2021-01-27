8845eceb481d5fdcef0ac0be60f0469fafa676f7
ElIsMail <M.El-Ismail@gmx.de>
2021-01-27 12:37:37 +0100

    "script changes"

--- Snakemake/submit_bbsplit_snakemake.sh
+++ Snakemake/submit_bbsplit_snakemake.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 SBATCH --job-name=bbsplit_snakemake
-SBATCH --output=logs/%x-%j.log
+SBATCH --output=/fast/users/${USER}/PI3K/Snakemake/logs/%x-%j.log
 SBATCH --ntasks=8
 SBATCH --nodes=1
 SBATCH --mem=64G
 
 
-export REFDIRM=/fast/users/${USER}/work/PIP3K/ref_files/Mouse
-export REFDIRH=/fast/users/${USER}/work/PIP3K/ref_files/Human
+#export REFDIRM=/fast/users/${USER}/work/PIP3K/ref_files/Mouse
+#export REFDIRH=/fast/users/${USER}/work/PIP3K/ref_files/Human
 export TMPDIR=/fast/users/${USER}/scratch/tmp/bbsplit
 export LOGDIR=/fast/users/${USER}/scratch/tmp/bbsplit/logs/${SLURM_JOB_NAME}-${SLURM_JOB_ID}
 mkdir -p $LOGDIR
