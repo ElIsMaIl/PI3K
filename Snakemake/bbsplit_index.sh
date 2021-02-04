bbab7bfb6488b07e550b01add993af59adf073fe
ElIsMaIl <M.El-Ismail@gmx.de>
2021-02-05 00:29:25 +0100

    changes

--- Snakemake/bbsplit_index.sh
+++ Snakemake/bbsplit_index.sh
@@ -3,6 +3,5 @@
 mkdir -p /fast/users/${USER}/scratch/tmp/bbsplit
 cd /fast/users/${USER}/scratch/tmp/bbsplit
 
-
 bbsplit.sh ref=/fast/users/${USER}/work/PIP3K/ref_files/Mouse/GRCm38.primary_assembly.genome.fa,/fast/users/${USER}/work/PIP3K/ref_files/Human/GRCh38.primary_assembly.genome.fa
 
