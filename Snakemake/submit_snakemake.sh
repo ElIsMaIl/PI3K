c9e1d318eab37f46f99b3e7882b4f21e1df70da4
ElIsMaIl <M.El-Ismail@gmx.de>
2021-02-19 11:31:06 +0100

    changed mem and latency wait

--- Snakemake/submit_snakemake.sh
+++ Snakemake/submit_snakemake.sh
@@ -13,9 +13,9 @@ set -x
 snakemake \
     --drmaa " \
     --nodes=1 \
-    --mem=64000 \
+    --mem=128000 \
     -n 8 \
     -o $LOGDIR/%x-%j.log" \
     -j 17 \
     -p \
-    --latency-wait 60 \
+    --latency-wait 120 \
