c4299a89014f26cc3c911fd1212b33dbdacb875b
Elismaim <M.El-Ismail@gmx.de>
2021-12-02 17:02:45 +0100

    changes
    Seurat_objects_new
    modifications on the rmd

--- Rscripts/Seurat_objects.R
+++ Rscripts/Seurat_objects.R
@@ -4,48 +4,30 @@ library(tidyverse)
 library(Seurat)
 
 
-# load data ----
-#Human
-fname <- list.files("data/Human_unfiltered/", pattern = "*.st.gz", full.names = T)
-names.exp <- list.files("data/Human_unfiltered/","*.st.gz", full.names = FALSE) %>% 
-  str_split(pattern = "_", simplify = T) %>% .[,1]
+# load data & prepare ----
+fnames <- list.files("data/Mouse_unfiltered/", pattern = "1820_Expression_Data_Unfiltered_mouse.st.gz", full.names = T)
+list.files("data/Mouse_unfiltered/","1820_Expression_Data_Unfiltered_mouse.st.gz", full.names = FALSE) %>% 
+  str_split(pattern = "_", simplify = T) %>% .[,1] -> names.exp
 
 
-list_seurat <- lapply(1:length(fname), function(id){
-  df_expression <- read.table(fname[id], sep= "\t", header=TRUE)
+list_seurat <- lapply(1:length(fnames), function(id){
+  df_expression <- read.table(fnames[id], sep= "\t", header=TRUE)
   df_expression <- df_expression[,-(3:4),drop=FALSE]
   
-sparse_matrix <- df_expression %>% 
+  df_expression %>% 
     mutate(Cell_Index = as.character(Cell_Index)) %>% 
-    dMcast(Gene~Cell_Index, value.var = "RSEC_Adjusted_Molecules")
+    dMcast(Gene~Cell_Index, value.var = "RSEC_Adjusted_Molecules") -> sparse_matrix
 
 exp <- CreateSeuratObject(sparse_matrix, project = names.exp[id], min.cells = 3, min.features = 200)
 exp[["percent.mt"]] <- PercentageFeatureSet(exp, pattern = "^MT-")
 
 return(exp)
 })
+names(list_seurat) <- names.exp
 
-#Mouse
-fname_mouse <- list.files("data/Mouse_unfiltered/", pattern = "*.st.gz", full.names = T)
-names.exp_mouse <- list.files("data/Mouse_unfiltered/","*.st.gz", full.names = FALSE) %>% 
-  str_split(pattern = "_", simplify = T) %>% .[,1]
-
-
-list_seurat <- lapply(1:length(fname_mouse), function(id){
-  df_expression <- read.table(fname_mouse[id], sep= "\t", header=TRUE)
-  df_expression <- df_expression[,-(3:4),drop=FALSE]
-  
-  sparse_matrix <- df_expression %>% 
-    mutate(Cell_Index = as.character(Cell_Index)) %>% 
-    dMcast(Gene~Cell_Index, value.var = "RSEC_Adjusted_Molecules")
-  
-  exp <- CreateSeuratObject(sparse_matrix, project = names.exp_mouse[id], min.cells = 3, min.features = 200)
-  exp[["percent.mt"]] <- PercentageFeatureSet(exp, pattern = "^MT-")
-  
-  return(exp)
-})
+## save seurat-objects ----
+save(list_seurat, file = "data/unfilteredSeurat_1820_mouse.Rdata")
 
 
-# save seurat-objects
-save(list_seurat, file = "data/unfilteredSeurat_full.Rdata")
-save(list_seurat, file = "data/unfilteredSeurat_full_mouse.Rdata")
+
+
--- Rscripts/Vergleichstabelle.R
+++ Rscripts/Vergleichstabelle.R
@@ -5,8 +5,9 @@ library("readr")
 
 ## load data ----
 # is there a more elegant way to import the data?
+#next step doesnot work - why?
 #fnames <- list.files("data/Gtf_Annotation/Human/", pattern = "*.gtf", full.names = T)
-# gtfs <- lapply(fnames, read_csv)
+# gtfs <- lapply(fnames, )
 
 
 # Human data
@@ -19,6 +20,7 @@ nlc_mouse <- as.data.frame(import("data/Gtf_Annotation/Mouse/gencode.vM19.long_n
 pri_mouse <- as.data.frame(import("data/Gtf_Annotation/Mouse/gencode.vM19.primary_assembly.annotation.gtf"))
 chr_mouse <- as.data.frame(import("data/Gtf_Annotation/Mouse/gencode.vM19.annotation.gtf"))
 
+
 ## select gene_type column and count the appearance of the different gene_types
 # Human
 gtf_nlc <- select(gtf_nlc, gene_type) %>% count(gene_type)
@@ -30,17 +32,27 @@ nlc_mouse <- select(nlc_mouse, gene_type) %>% count(gene_type)
 pri_mouse <- select(pri_mouse, gene_type) %>% count(gene_type)
 chr_mouse <- select(chr_mouse, gene_type) %>% count(gene_type)
 
+#listofdf_human <- list(gtf_nlc, gtf_pri, gtf_chr)
+#table_gtfs <- listofdf_human %>% 
+#  filter("gene_type" %in% c(gtf_nlc$gene_type, gtf_pri$gene_type, gtf_chr$gene_type))
+#  select("gtf_nlc$gene_type", "gtf_pri$gene_type", "gtf_chr$gene_type") %>%
+#  count("gtf_nlc$gene_type", "gtf_pri$gene_type", "gtf_chr$gene_type") %>%
+#  rbind(listofdf_human)
+
+#listofdf_mouse <- list(gtf_nlc, gtf_pri, gtf_chr)
+
+
 ## combine dataframes to one table ----
 # Human
-table_gtfs <- full_join(gtf_nlc, gtf_pri, by = 'gene_type', all=TRUE) %>%
- full_join(., gtf_chr, by = 'gene_type', all=TRUE)
+table_gtfs <- cbind(gtf_nlc, gtf_pri, by = 'gene_type', all=TRUE) %>%
+  cbind(., gtf_chr, by = 'gene_type', all=TRUE)
 
 names(table_gtfs) <- c("Gene_Type", "gtf_nlc", "gtf_pri", "gtf_chr")
 
 # Mouse
 
-table_mouse <- full_join(nlc_mouse, pri_mouse, by = 'gene_type', all=TRUE) %>%
-  full_join(., chr_mouse, by='gene_type', all=TRUE)
+table_mouse <- cbind(nlc_mouse, pri_mouse, by = 'gene_type', all=TRUE) %>%
+  cbind(., chr_mouse, by='gene_type', all=TRUE)
 
 names(table_mouse) <- c("Gene_Type", "gtf_nlc", "gtf_pri", "gtf_chr")
 
@@ -56,5 +68,5 @@ names(table_mouse) <- c("Gene_Type", "gtf_nlc", "gtf_pri", "gtf_chr")
 
 ## safe table
 
-write.csv2(table_gtfs, file='E:\\PI3k/data/Gtf_Annotation/gtf_table.csv')
-write.csv2(table_mouse, file='E:\\PI3K/data/Gtf_Annotation/gtf_table_mouse.csv')
+write.table(table_gtfs, file='E:\\PI3k/data/Gtf_Annotation/gtf_table.csv')
+write.table(table_mouse, file='E:\\PI3K/data/Gtf_Annotation/gtf_table_mouse.csv')
