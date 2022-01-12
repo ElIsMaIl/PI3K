28dbaa3d830fa405e866a3cc2553a2e07760a94a
Elismaim <M.El-Ismail@gmx.de>
2022-01-12 16:40:25 +0100

    changes in RMD and Seurat_objects.R
    Legends and captions were added to figures and tables in the RMD.
    
    renamed Seurat_objects in Seurat_objects.R

--- Rscripts/Seurat_objects.R
+++ Rscripts/Seurat_objects.R
@@ -10,7 +10,7 @@ list.files("data/Mouse_unfiltered/","1820_Expression_Data_Unfiltered_mouse.st.gz
   str_split(pattern = "_", simplify = T) %>% .[,1] -> names.exp
 
 
-list_seurat <- lapply(1:length(fnames), function(id){
+list_seurat_1820m <- lapply(1:length(fnames), function(id){
   df_expression <- read.table(fnames[id], sep= "\t", header=TRUE)
   df_expression <- df_expression[,-(3:4),drop=FALSE]
   
@@ -23,11 +23,33 @@ exp[["percent.mt"]] <- PercentageFeatureSet(exp, pattern = "^MT-")
 
 return(exp)
 })
-names(list_seurat) <- names.exp
+names(list_seurat_1820m) <- names.exp
 
 ## save seurat-objects ----
-save(list_seurat, file = "data/unfilteredSeurat_1820_mouse.Rdata")
+save(list_seurat_1820m, file = "data/unfilteredSeurat_1820_mouse.Rdata")
 
 
 
+# load data & prepare ----
+fnames <- list.files("data/Mouse_unfiltered/", pattern = "1822_Expression_Data_Unfiltered_mouse.st.gz", full.names = T)
+list.files("data/Mouse_unfiltered/","1822_Expression_Data_Unfiltered_mouse.st.gz", full.names = FALSE) %>% 
+  str_split(pattern = "_", simplify = T) %>% .[,1] -> names.exp
 
+
+list_seurat_1822m <- lapply(1:length(fnames), function(id){
+  df_expression <- read.table(fnames[id], sep= "\t", header=TRUE)
+  df_expression <- df_expression[,-(3:4),drop=FALSE]
+  
+  df_expression %>% 
+    mutate(Cell_Index = as.character(Cell_Index)) %>% 
+    dMcast(Gene~Cell_Index, value.var = "RSEC_Adjusted_Molecules") -> sparse_matrix
+  
+  exp <- CreateSeuratObject(sparse_matrix, project = names.exp[id], min.cells = 3, min.features = 200)
+  exp[["percent.mt"]] <- PercentageFeatureSet(exp, pattern = "^MT-")
+  
+  return(exp)
+})
+names(list_seurat_1822m) <- names.exp
+
+## save seurat-objects ----
+save(list_seurat_1822m, file = "data/unfilteredSeurat_1822_mouse.Rdata")
