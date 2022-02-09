deed16e94acf201879a27f4687ce203ffa48da52
Elismaim <M.El-Ismail@gmx.de>
2022-02-09 17:19:14 +0100

    updated RMDs
    Overlap between cells and genes added

--- Rscripts/Seurat_objects.R
+++ Rscripts/Seurat_objects.R
@@ -5,12 +5,12 @@ library(Seurat)
 
 
 # load data & prepare ----
-fnames <- list.files("data/Mouse_unfiltered/", pattern = "7271_Expression_Data_Unfiltered_mouse.st.gz", full.names = T)
-list.files("data/Mouse_unfiltered/","7271_Expression_Data_Unfiltered_mouse.st.gz", full.names = FALSE) %>% 
+fnames <- list.files("data/Mouse_unfiltered/", pattern = "7272_Expression_Data_Unfiltered_mouse.st.gz", full.names = T)
+list.files("data/Mouse_unfiltered/","7272_Expression_Data_Unfiltered_mouse.st.gz", full.names = FALSE) %>% 
   str_split(pattern = "_", simplify = T) %>% .[,1] -> names.exp
 
 
-list_seurat_7271m <- lapply(1:length(fnames), function(id){
+list_seurat_7272m <- lapply(1:length(fnames), function(id){
   df_expression <- read.table(fnames[id], sep= "\t", header=TRUE)
   df_expression <- df_expression[,-(3:4),drop=FALSE]
   
@@ -23,22 +23,22 @@ exp[["percent.mt"]] <- PercentageFeatureSet(exp, pattern = "^mt-")
 
 return(exp)
 })
-names(list_seurat_7271m) <- names.exp
+names(list_seurat_7272m) <- names.exp
 
-str(list_seurat_7271m)
+str(list_seurat_7272m)
 
 ## save seurat-objects ----
-save(list_seurat_7271m, file = "data/unfilteredSeurat_7271_mouse.Rdata")
+save(list_seurat_7272m, file = "data/unfilteredSeurat_7272_mouse.Rdata")
 
 
 
 # load data & prepare ----
-fnames <- list.files("data/7271_unfiltered/", pattern = "*.st.gz", full.names = T)
-list.files("data/7270_unfiltered/","*.st.gz", full.names = FALSE) %>% 
+fnames <- list.files("data/7272_unfiltered/", pattern = "*.st.gz", full.names = T)
+list.files("data/7272_unfiltered/","*.st.gz", full.names = FALSE) %>% 
   str_split(pattern = "_", simplify = T) %>% .[,1] -> names.exp
 
 
-list_seurat_7271 <- lapply(1:length(fnames), function(id){
+list_seurat_7272 <- lapply(1:length(fnames), function(id){
   df_expression <- read.table(fnames[id], sep= "\t", header=TRUE)
   df_expression <- df_expression[,-(3:4),drop=FALSE]
   
@@ -51,8 +51,8 @@ list_seurat_7271 <- lapply(1:length(fnames), function(id){
   
   return(exp)
 })
-names(list_seurat_7271) <- names.exp
+names(list_seurat_7272) <- names.exp
 
-str(list_seurat_7271)
+str(list_seurat_7272)
 ## save seurat-objects ----
-save(list_seurat_7271, file = "data/unfilteredSeurat_7271.Rdata")
+save(list_seurat_7272, file = "data/unfilteredSeurat_7272.Rdata")
